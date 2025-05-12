44d5a80a92a582ee8c016c85d0d184addd36e945
Sharon <omuwaste@gmail.com>
2025-05-12 10:01:24 +0100

    add google tag manager to blog site

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { GoogleTagManager } from "@next/third-parties/google";
 
 export const metadata: Metadata = {
   title: "Blog | Unitellas International Limited",
@@ -13,7 +14,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`antialiased w-full overflow-x-hidden`}>{children}</body>
+      <body className={`antialiased w-full overflow-x-hidden`}>
+        <GoogleTagManager gtmId="GTM-MRB2FRFG" />
+        {children}
+      </body>
     </html>
   );
 }
